85c1e91a707d064914fe96717011903510efad33
sd4 <soumikd4@gmail.com>
2020-08-22 20:21:49 +0530

    broadcast

--- stock-server.js
+++ stock-server.js
@@ -8,18 +8,35 @@ const randomStockObject = require("./random-stock-values")
 
 const port = process.env.PORT || 9090;
 
-io.on("connection", socket => {
-    getStockValues(socket);
+io.of("/live")
+	.on("connection", socket => {
+	socket.join("stocks");
+    socket.on("joinStocksRoom", room => {
+    	if (room == "stocks") {
+    		socket.join("stocks");
+    	}
+    });
     setInterval(() => {
-        getStockValues(socket);
-    }, 5000);
+    	io.of("/live").to("stocks").emit("livestocks",[
+			randomStockObject.getAppleStockValues(),
+			randomStockObject.getGoogleStockValues(),
+			randomStockObject.getMicrosoftStockValues()
+		])           
+    }, 2000);
 })
 
-function getStockValues(socket) {
-    socket.emit("appleStocks", randomStockObject.getAppleStockValues());
-    socket.emit("googleStocks", randomStockObject.getGoogleStockValues());
-    socket.emit("microsoftStocks", randomStockObject.getMicrosoftStockValues());
+// io.on("connection", socket => {
+//     getStockValues(socket);
+//     setInterval(() => {
+//         getStockValues(socket);
+//     }, 5000);
+// })
 
-}
+// function getStockValues(socket) {
+//     socket.emit("appleStocks", randomStockObject.getAppleStockValues());
+//     socket.emit("googleStocks", randomStockObject.getGoogleStockValues());
+//     socket.emit("microsoftStocks", randomStockObject.getMicrosoftStockValues());
+
+// }
 
 http.listen(port, ()=> console.log(`the stock server is listening on port - ${port}`));
